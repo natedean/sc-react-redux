79e98790cd06151dab5ed7d72c2702e9b499c225
Nate Dean <nate.dean@pathar.net>
2016-06-26 16:08:25 -0500

    Initial redux app commit

---
+++ src/reducers/track.js
@@ -0,0 +1,16 @@
+import * as actionTypes from '../constants/actionTypes';
+
+export default (state = [], action) => {
+    switch (action.type) {
+        case actionTypes.TRACKS_SET:
+            return setTracks(state, action);
+    }
+
+    return state;
+}
+
+function setTracks(state, action) {
+    const { tracks } = action;
+
+    return [ ...state, ...tracks ];
+}
